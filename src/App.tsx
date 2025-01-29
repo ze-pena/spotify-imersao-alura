0ca8b9159a52ec3ed5b87919d222ab837e8ce06b
ze-pena <jose.pena.dev@gmail.com>
2025-01-28 22:14:42 -0300

    refactor: apply router configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+// Roteamento
+import { Routes, Route } from 'react-router-dom'
+
+// Layouts
+import DefaultLayout from './layouts/DefaultLayout'
+
+// Páginas
+import Home from './pages/Home'
+
+// Estilização
 import './App.css'
 
 export default function App() {
-  return (
-    <>
-      <h1>Hellor world!</h1>
-    </>
+  return(
+    <Routes>
+      <Route element={<DefaultLayout/>}>
+        <Route index element={<Home/>}/>
+      </Route>
+    </Routes>
   )
 }
