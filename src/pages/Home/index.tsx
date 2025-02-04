bf50812ca64b854570bea1e0588a21ce4af4b980
ze-pena <jose.pena.dev@gmail.com>
2025-02-03 21:51:01 -0300

    feat: call the artist service into home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,33 @@
-import { ReactNode } from "react";
+import { ReactNode, useCallback, useEffect } from "react"
+
+import artistService from "../../services/artist"
+
+import ArtistCard from "../../components/cards/ArtistCard"
+import { useArtistContext } from "../../contexts/Artist/context"
+
 import "./styles.css"
 
 export default function Home(): ReactNode {
+  const { context, setContext } = useArtistContext()
+  
+  const getArtistList = useCallback(async () => {
+    try {
+      const response = await artistService.getArtistList()
+      setContext(e => ({ ...e, artistList: [...response] }))
+    } catch {
+      setContext(e => ({ ...e, artistList: [] }))
+    }
+  }, [setContext])
+
+  useEffect(() => {
+    if (!context.artistList.length) getArtistList()
+  }, [context, getArtistList])
+  
   return(
     <div className="home">
+      {context.artistList.map(artist => (
+        <ArtistCard key={artist.id} artist={artist} />
+      ))}
     </div>
   )
 }
