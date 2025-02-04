9eecc0c7f508227039437a0bc528a3a7c95c44c7
ze-pena <jose.pena.dev@gmail.com>
2025-02-03 21:51:23 -0300

    feat: create artist service

---
+++ src/services/artist.ts
@@ -0,0 +1,18 @@
+type GET_ARTISTS_PARAMS = {
+  search: string
+}
+
+type GET_ARTISTS_RESPONSE = Artist[]
+
+const baseUrl = "http://127.0.0.1:4001"
+
+export default {
+  getArtistList: async (params?: GET_ARTISTS_PARAMS): Promise<GET_ARTISTS_RESPONSE> => {
+    let query = ""
+
+    if (params && params.search) query = `?name_like=${params.search.toLowerCase()}`
+
+    const request = await fetch(`${baseUrl}/artists${query}`)
+    return request.json()
+  }
+}
